830fedd3251ea2f744d18661e5f49d9dc912d55e
Avnoor <54592053+avnoor-488@users.noreply.github.com>
2021-09-16 14:44:42 +0530

    Add files via upload

---
+++ authy/models.py
@@ -0,0 +1,16 @@
+from django.db import models
+
+# Create your models here.
+
+# class user_datas(models.Model):
+#     username = models.CharField(max_length=50)
+#     email = models.CharField(max_length=50)
+#     name = models.CharField(max_length=50)
+#     age = models.CharField(max_length=50)
+#     user_field = models.CharField(max_length=50)
+#     user_profession = models.CharField(max_length=50)
+#     user_university = models.CharField(max_length=50)
+#     password = models.CharField(max_length=200)
+#     password2 = models.CharField(max_length=200)	
+#     def __str__(self):
+#         return self.name
